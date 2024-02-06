ae7240d6772a381e71a52274361804d3a9fe1e1b
Nathan Thomas <nathanthomas707@gmail.com>
2024-02-06 10:12:10 -0800

    Implement augmentation (#24)

--- darrennet/main.py
+++ darrennet/main.py
@@ -1,10 +1,10 @@
 import click
 import numpy as np
 import torch
-import torchvision.transforms as standard_transforms
 from click_help_colors import HelpColorsCommand, HelpColorsGroup
 from rich.traceback import install
 from torch import nn
+from torchvision.transforms import v2
 
 from . import theme
 from .basic_fcn import FCN
@@ -13,10 +13,6 @@ from .train import model_test, model_train
 
 install(show_locals=True)
 
-mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-input_transform = standard_transforms.Compose(
-    [standard_transforms.ToTensor(), standard_transforms.Normalize(*mean_std)]
-)
 
 epochs = 20
 n_class = 21
@@ -63,23 +59,60 @@ def download():
     download_data()
 
 
+@click.option("-a", "--augment", help="Choose <=4 from {a,v,h,r}")
 @main.command(cls=HelpColorsCommand)
-def cook():
+def cook(augment):
     """Train the model."""
     device = find_device()
     fcn_model = FCN(n_class=n_class)
     fcn_model.apply(init_weights)
 
-    # TODO determine which device to use (cuda or cpu)
-    # Check that MPS is available
-    optimizer = torch.optim.Adam(params=fcn_model.parameters(), lr=0.001)
+    augment_transforms: list[v2.Transform] = [v2.ToImage()]
+    if augment is not None:
+        if "a" in augment:
+            theme.print("Affine transform selected.")
+            augment_transforms.append(v2.RandomAffine((-20, 20), translate=(0, 0.2)))
+        if "v" in augment:
+            theme.print("Vertical flip transform selected.")
+            augment_transforms.append(v2.RandomVerticalFlip(0.5))
+        if "h" in augment:
+            theme.print("Horizontal flip transform selected.")
+            augment_transforms.append(v2.RandomHorizontalFlip(0.5))
+        if "r" in augment:
+            theme.print("Resized Crop transform selected.")
+            augment_transforms.append(v2.RandomResizedCrop((224, 224)))
 
+    augment_transform = v2.Compose(augment_transforms)
+
+    optimizer = torch.optim.Adam(params=fcn_model.parameters(), lr=0.001)
     criterion = torch.nn.CrossEntropyLoss()
 
     fcn_model = fcn_model.to(device)
-    train_loader, val_loader, test_loader = load_dataset(
-        input_transform, MaskToTensor()
+    mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    # Final transform for input
+    input_transform = v2.Compose(
+        [
+            v2.ToImage(),
+            v2.Resize((224, 224)),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(*mean_std),
+        ]
     )
+    mask_transform = v2.Compose(
+        [
+            v2.ToImage(),
+            v2.Resize((224, 224)),
+            # Change shape from (16,1,224,224) -> (16,224,224)
+            v2.Lambda(lambda x: torch.squeeze(x, dim=0)),
+        ]
+    )
+
+    train_loader, val_loader, test_loader = load_dataset(
+        augment_transform,
+        input_transform,
+        mask_transform,
+    )
+
     theme.print("Training network...")
     model_train(
         fcn_model, optimizer, criterion, device, train_loader, val_loader, epochs
